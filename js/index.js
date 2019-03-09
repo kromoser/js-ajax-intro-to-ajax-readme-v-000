151e9332de457912656515edd060f45e653ee17d
kromoser <kevinromoser@gmail.com>
2019-03-09 16:23:28 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/kromoser/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText)
+  //console.log(repos)
+  const repoList = `<ul>${repos
+    .map(
+      repo =>
+        '<li>' +
+        repo.name +
+        ' - <a href="#" data-repo="' +
+        repo.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+      )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/kromoser/' + name + '/commits');
+  req.send();
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  console.log(commits)
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+      '<li><strong>' +
+      commit.commit.author.name +
+      '</strong> - ' +
+      commit.commit.message +
+      '</li>'
+    )
+    .join('')}</ul>`;
+    document.getElementById('commits').innerHTML = commitsList;
+}
